79b43d1cec233aec7e288672b86eac2671fbdc49
Leonardo Rodriguez-Velez <royer@boxymoron.com>
2018-02-07 19:11:06 -0500

    fix classpath

--- bin/backend.sh
+++ bin/backend.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ./lib/Layer7Router.jar -Xms1G -Xmx2G -XX:+AlwaysPreTouch -XX:MaxDirectMemorySize=8G -XX:+UseParallelGC -Djava.net.preferIPv4Stack=true Layer7RouterBackend -num_threads 4 -client_base_ip 192.168.1 -buffer_size 1024
+java -cp ../lib/Layer7Router.jar -Xms1G -Xmx2G -XX:+AlwaysPreTouch -XX:MaxDirectMemorySize=8G -XX:+UseParallelGC -Djava.net.preferIPv4Stack=true Layer7RouterBackend -num_threads 4 -client_base_ip 192.168.1 -buffer_size 1024
