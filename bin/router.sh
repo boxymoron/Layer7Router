166ffb53aff65314fc674b139f499f3a72178f2d
Leonardo Rodriguez-Velez <royer@boxymoron.com>
2018-02-07 19:12:37 -0500

    fix classpath

--- bin/router.sh
+++ bin/router.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -server -Xmx4G -XX:+AlwaysPreTouch -XX:MaxDirectMemorySize=8G -XX:+UseParallelGC -Djava.net.preferIPv4Stack=true -jar lib/Layer7Router.jar -backend_host 127.0.0.1 -backend_port 7180
+java -server -Xmx4G -XX:+AlwaysPreTouch -XX:MaxDirectMemorySize=8G -XX:+UseParallelGC -Djava.net.preferIPv4Stack=true -jar ../lib/Layer7Router.jar -backend_host 127.0.0.1 -backend_port 7180
